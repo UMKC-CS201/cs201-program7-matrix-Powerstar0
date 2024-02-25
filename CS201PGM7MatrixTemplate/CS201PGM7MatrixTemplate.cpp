8e90f5e101ce699c25517fa6cd677a328ec82034
Powerstar0 <133736954+Powerstar0@users.noreply.github.com>
2024-02-25 04:03:26 -0600

    Finish

--- CS201PGM7MatrixTemplate/CS201PGM7MatrixTemplate.cpp
+++ CS201PGM7MatrixTemplate/CS201PGM7MatrixTemplate.cpp
@@ -1,9 +1,11 @@
-// CS201PGM7Matrix 
-// NAME:  
-//
+// PROGRAM: CS201PGM7Matrix
+// NAME: Johnny Diep
+// DATE: 02/25/2024
+// Purpose: Read various matrices and calculate several properties.
 
 #include <iostream>
 #include "Matrix.h"
+#include <string>
 using namespace std;
 
 
@@ -17,13 +19,38 @@ int main()
     cout << "\n***************************************************" << endl;
 
     //open file & check it is open
+    ifstream inStream;
+    inStream.open(R"(C:\Users\Johnny\CLionProjects\cs201-program7-matrix-Powerstar0\CS201PGM7MatrixTemplate\data.txt)"); //String literal from CLION
+    if (!inStream.is_open()){
+        cout << "Error opening files" << endl;
+
+        return 1;
+    }
     //if so, loop through each record in the input file:
     //   load the new matrix
     //   if load is successful (return == 1)
     //      process the matrix
+    int vector_number = 1;
+    while (inStream.good()) {
+        int vectorSize;
+        inStream >> vectorSize;
+        vector<vector<int>> matrix;
+        //resizes vector
+        matrix.resize(vectorSize);
+        for (int i = 0; i < vectorSize; ++i) {
+            matrix.at(i).resize(vectorSize);
+        }
+        //loads matrix if its valid
+        if (loadMatrix(inStream, matrix, vectorSize, vector_number) == 1) {
+            //printVector(matrix, vectorSize);
+            processMatrix(matrix, vectorSize, vector_number);
+        }
+        //Increments vector number
+        vector_number++;
+    }
 
     //close the input file
-
+    inStream.close();
     //wait to view any output printed to the screen (if needed)
     string hold;
     cin >> hold;
